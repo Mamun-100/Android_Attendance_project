3234ea65ec9505a60f62a4da7cdc02274a7028d8
Mamun-100 <rashid202412@gmail.com>
2021-02-23 23:54:51 +0600

    layout design New add branch

--- app/src/main/java/com/pro/attendanceapp/MainActivity.java
+++ app/src/main/java/com/pro/attendanceapp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.pro.attendanceapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab_main);
+        fab.setOnClickListener(v->showDialog());
+
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().from(this).inflate(R.layout.branch_dialog,null);
+        builder.setView(view);
+        builder.create().show();;
     }
 }
